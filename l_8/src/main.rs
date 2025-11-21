a2080da41849b8dff3ffc9b9cc45a7f3bd0ad25c
1180779 <rdkgsk@gmail.com>
2025-11-21 12:33:26 +0100

    Add AustroHungarianGreeter

--- l_8/src/main.rs
+++ l_8/src/main.rs
@@ -1,3 +1,36 @@
+use std::cell::Cell;
+
+struct AustroHungarianGreeter {
+    greeting: Cell<i8>,
+    greetings_invoked: Cell<u32>,
+}
+
+impl AustroHungarianGreeter {
+    fn greet(&self) -> &str {
+        self.greetings_invoked.set(self.greetings_invoked.get() + 1);
+        match self.greeting.get() {
+            0 => {
+                self.greeting.set(1);
+                "Es lebe der Kaiser!"
+            },
+            1 => {
+                self.greeting.set(2);
+                "Möge uns der Kaiser schützen!"
+            },
+            _ => {
+                self.greeting.set(0);
+                "Éljen Ferenc József császár!"
+            }
+        }
+    }
+}
+
+impl Drop for AustroHungarianGreeter {
+    fn drop(&mut self) {
+        println!("Ich habe {} mal gegrüßt", self.greetings_invoked.get());
+    }
+}
+
 fn main() {
 
     println!("Hello, world!");
