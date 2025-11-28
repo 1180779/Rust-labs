415531c11bd031cc0f0a1491843aa1282c189f2e
1180779 <rdkgsk@gmail.com>
2025-11-28 10:18:23 +0100

    Tests

--- l_8/src/main.rs
+++ l_8/src/main.rs
@@ -1,8 +1,8 @@
 use std::borrow::Cow;
-use std::cell::{Cell, LazyCell, OnceCell, RefCell, RefMut};
+use std::cell::{Cell, LazyCell, OnceCell, RefCell};
 use std::collections::VecDeque;
 use std::fs;
-use std::io::read_to_string;
+use std::io::Write;
 use std::ops::{Deref, DerefMut};
 use std::path::{Path, PathBuf};
 use std::rc::{Rc, Weak};
@@ -13,6 +13,12 @@ struct AustroHungarianGreeter {
 }
 
 impl AustroHungarianGreeter {
+    fn new() -> Self {
+        AustroHungarianGreeter {
+            greeting: Cell::new(0),
+            greetings_invoked: Cell::new(0),
+        }
+    }
     fn greet(&self) -> &str {
         self.greetings_invoked.set(self.greetings_invoked.get() + 1);
         match self.greeting.get() {
@@ -65,7 +71,7 @@ impl<T> DerefMut for HeapOrStack<T> {
 
 pub fn canon_head<'a>(xs: &'a VecDeque<i32>) -> Option<Cow<'a, VecDeque<i32>>> {
     let mut cow = Cow::Borrowed(xs);
-    for i in 0..xs.len() {
+    for _i in 0..xs.len() {
         let front = cow.front();
         let front = front?;
         if front % 2 == 1 {
@@ -198,17 +204,157 @@ impl Vertex {
     /// Zadbaj o to, aby cykl nie powodował wycieków pamięci!
     /// (odpowiednio używaj `create_neighbour` i `link_to`)
     pub fn cycle(n: usize) -> Rc<RefCell<Vertex>> {
-        let mut first_rc: Rc<RefCell<Vertex>> = Rc::new(RefCell::new(Vertex::new()));
-        let mut last = first_rc.borrow_mut();
-        for i in 0..n {
-            let neighbour = last.create_neighbour();
-            last = neighbour.borrow_mut();
+        let first_rc: Rc<RefCell<Vertex>> = Rc::new(RefCell::new(Vertex::new()));
+        let mut current_rc = Rc::clone(&first_rc);
+
+        for i in 1..n {
+            let new_neighbour_rc = current_rc.borrow_mut().create_neighbour();
+            new_neighbour_rc.borrow_mut().data = i as i32;
+            current_rc = new_neighbour_rc;
         }
-        last.link_to(&first_rc);
+
+        current_rc.borrow_mut().link_to(&first_rc);
         first_rc
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    // AustroHungarianGreeter
+    let greeter = AustroHungarianGreeter::new();
+    greeter.greet();
+
+    // SharedFile
+    let path = Path::new("test.txt");
+    let mut f = fs::File::create(path).unwrap();
+    f.write_all(b"Hello, world!").unwrap();
+    let file = SharedFile::new(path.into());
+    println!("file content: {}", file.get());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::io::Write;
+
+    #[test]
+    fn test_greeter() {
+        let greeter = AustroHungarianGreeter::new();
+        assert_eq!(greeter.greet(), "Es lebe der Kaiser!");
+        assert_eq!(greeter.greet(), "Möge uns der Kaiser schützen!");
+        assert_eq!(greeter.greet(), "Éljen Ferenc József császár!");
+        assert_eq!(greeter.greet(), "Es lebe der Kaiser!");
+        assert_eq!(greeter.greetings_invoked.get(), 4);
+    }
+
+    #[test]
+    fn test_heap_or_stack() {
+        let mut stack_val = HeapOrStack::Stack(42);
+        let mut heap_val = HeapOrStack::Heap(Box::new(42));
+
+        assert_eq!(*stack_val, 42);
+        assert_eq!(*heap_val, 42);
+
+        *stack_val = 43;
+        *heap_val = 43;
+
+        assert_eq!(*stack_val, 43);
+        assert_eq!(*heap_val, 43);
+    }
+
+    #[test]
+    fn test_canon_head() {
+        let dq = VecDeque::from(vec![2, 4, 6, 1, 8]);
+        let cow = canon_head(&dq).unwrap();
+        assert_eq!(*cow, VecDeque::from(vec![1, 8, 2, 4, 6]));
+        assert!(matches!(cow, Cow::Owned(_)));
+
+        let dq2 = VecDeque::from(vec![1, 2, 3, 4, 5]);
+        let cow2 = canon_head(&dq2).unwrap();
+        assert_eq!(*cow2, dq2);
+        assert!(matches!(cow2, Cow::Borrowed(_)));
+
+        let dq3 = VecDeque::from(vec![2, 4, 6, 8]);
+        let cow3 = canon_head(&dq3).unwrap();
+        assert_eq!(*cow3, VecDeque::from(vec![2, 4, 6, 8]));
+
+        let dq4 = VecDeque::new();
+        assert!(canon_head(&dq4).unwrap().is_empty());
+    }
+
+    #[test]
+    fn test_cached_file() {
+        let file = CachedFile::new();
+        assert_eq!(file.try_get(), None);
+
+        let path = Path::new("test.txt");
+        let mut f = fs::File::create(path).unwrap();
+        f.write_all(b"Hello, world!").unwrap();
+
+        assert_eq!(file.try_get(), None);
+        assert_eq!(file.get(path), "Hello, world!");
+        assert_eq!(file.try_get(), Some("Hello, world!"));
+        assert_eq!(file.get(path), "Hello, world!");
+    }
+
+    #[test]
+    fn test_shared_file() {
+        let path = PathBuf::from("shared_test.txt");
+        let mut f = fs::File::create(&path).unwrap();
+        f.write_all(b"Shared content").unwrap();
+
+        let file1 = SharedFile::new(path.clone());
+        let file2 = file1.clone();
+
+        assert_eq!(file1.get(), "Shared content");
+        assert_eq!(file2.get(), "Shared content");
+
+        assert_eq!(file1.get().as_ptr(), file2.get().as_ptr());
+
+        fs::remove_file(path).unwrap();
+    }
+
+    #[test]
+    fn test_vertex() {
+        let mut v1 = Vertex::new();
+        v1.data = 1;
+        let v2 = v1.create_neighbour();
+        v2.borrow_mut().data = 2;
+        let v3 = v1.create_neighbour();
+        v3.borrow_mut().data = 3;
+
+        v2.borrow_mut().link_to(&v3);
+
+        assert_eq!(v1.out_edges_owned.len(), 2);
+        assert_eq!(v1.out_edges.len(), 0);
+        assert_eq!(v2.borrow().out_edges_owned.len(), 0);
+        assert_eq!(v2.borrow().out_edges.len(), 1);
+
+        let neighbours = v1.all_neighbours();
+        assert_eq!(neighbours.len(), 2);
+    }
+
+    #[test]
+    fn test_vertex_cycle(){
+        let cycle_head = Vertex::cycle(5);
+        let mut current = Rc::downgrade(&cycle_head);
+        for i in 0..5 {
+            assert_eq!(current.upgrade().unwrap().borrow().data, i);
+            let next_strong = current.upgrade().unwrap().borrow().all_neighbours()[0].clone();
+            current = next_strong;
+        }
+
+        assert!(Weak::ptr_eq(&Rc::downgrade(&current.upgrade().unwrap()), &Rc::downgrade(&cycle_head)));
+    }
+    
+    #[test]
+    fn test_vertex_cycle_memory() {
+        let n = 10;
+        let head_rc = Vertex::cycle(n);
+        let head_weak = Rc::downgrade(&head_rc);
+
+        drop(head_rc);
+
+        assert!(head_weak.upgrade().is_none(), "Cycle was not properly deallocated!");
+    }
 }
